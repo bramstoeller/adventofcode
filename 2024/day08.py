b563fbf2b1efc808ceb60762ad5cce57fc083f37
Bram Stoeller <bram@stoeller.nl>
2024-12-08 21:04:51 +0100

    Refactor Day 8

--- 2024/day08.py
+++ 2024/day08.py
@@ -1,58 +1,53 @@
 # Advent of Code 2024, Day 8
 # https://adventofcode.com/2024/day/8
-from itertools import combinations
 
 
 # Part One
+from collections import defaultdict
+
+
 def read_map(file_name):
-    data = open(file_name, "r").read().strip().splitlines()
-    antennas = {(x, y): c for y, line in enumerate(data) for x, c in enumerate(line) if c != "."}
-    antennas = {antenna: [p for p, a in antennas.items() if a == antenna] for antenna in set(antennas.values())}
-    max_x = len(data[0]) - 1
-    max_y = len(data) - 1
-    return antennas, max_x, max_y
+    data = [line.strip() for line in open(file_name)]
+    antennas = defaultdict(list)
+    for x, y, c in ((x, y, c) for y, line in enumerate(data) for x, c in enumerate(line) if c != "."):
+        antennas[c].append(x + y * 1j)
+    return dict(antennas), (len(data[0]) - 1) + (len(data) - 1) * 1j
 
 
-def find_anti_antennas(antennas):
-    anti_antennas = set()
-    for antenna, points in antennas.items():
-        for p, q in combinations(points, 2):
-            dx = p[0] - q[0]
-            dy = p[1] - q[1]
-            anti_antennas.add((p[0] + dx, p[1] + dy))
-            anti_antennas.add((q[0] - dx, q[1] - dy))
-    return anti_antennas
+def on_map(p, p_max):
+    return 0 <= p.real <= p_max.real and 0 <= p.imag <= p_max.imag
+
+
+def find_anti_antennas(antennas, p_max):
+    anti_antennas = {p - (q - p) for antenna, points in antennas.items() for p, q in combinations(points, 2)}
+    anti_antennas |= {q + (q - p) for antenna, points in antennas.items() for p, q in combinations(points, 2)}
+    return {p for p in anti_antennas if on_map(p, p_max)}
 
 
 def part_1(file_name):
-    antennas, max_x, max_y = read_map(file_name)
-    return len(set((x, y) for x, y in find_anti_antennas(antennas) if 0 <= x <= max_x and 0 <= y <= max_y))
+    return len(find_anti_antennas(*read_map(file_name)))
 
 
 # Part Two
-def find_all_anti_antennas(antennas, max_x, max_y):
-    anti_antennas = set()
-    for antenna, points in antennas.items():
-        for p, q in combinations(points, 2):
-            dx = p[0] - q[0]
-            dy = p[1] - q[1]
-            x = p[0]
-            y = p[1]
-            while 0 <= x <= max_x and 0 <= y <= max_y:
-                anti_antennas.add((x, y))
-                x += dx
-                y += dy
-            x = q[0]
-            y = q[1]
-            while 0 <= x <= max_x and 0 <= y <= max_y:
-                anti_antennas.add((x, y))
-                x -= dx
-                y -= dy
-    return anti_antennas
+from itertools import chain, combinations
+
+
+def pt_range(p, q, p_max):
+    d = q - p
+    n1 = min(p.real // d.real if d.real > 0 else (p_max.real - p.real) // -d.real,
+             p.imag // d.imag if d.imag > 0 else (p_max.imag - p.imag) // -d.imag)
+    n2 = min((p_max.real - p.real) // d.real if d.real > 0 else p.real // -d.real,
+             (p_max.imag - p.imag) // d.imag if d.imag > 0 else p.imag // -d.imag)
+    return {p + n * d for n in range(-int(n1), int(n2 + 1))}
+
+
+def find_all_anti_antennas(antennas, max_p):
+    anti_antennas = (pt_range(p, q, max_p) for _, points in antennas.items() for p, q in combinations(points, 2))
+    return set(chain(*anti_antennas))
 
 
 def part_2(file_name):
-    return len(set(find_all_anti_antennas(*read_map(file_name))))
+    return len(find_all_anti_antennas(*read_map(file_name)))
 
 
 if __name__ == "__main__":
