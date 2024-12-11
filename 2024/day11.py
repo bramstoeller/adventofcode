4afd899b78f0a9627a7d4ecea74f354df21d8c58
Bram Stoeller <bram@stoeller.nl>
2024-12-11 21:55:16 +0100

    Day 11 Refactor

--- 2024/day11.py
+++ 2024/day11.py
@@ -1,66 +1,56 @@
 # Advent of Code 2024, Day 11
 # https://adventofcode.com/2024/day/11
 
-
 # Part One
+from itertools import chain
+
+
 def load_data(file_name):
-    return list(map(int, open(file_name, "r").read().strip().split()))
+    return map(int, open(file_name, "r").read().strip().split())
 
 
-def blink(stones):
-    out = []
-    for s in stones:
-        if s == 0:
-            out.append(1)
-        elif len(str(s)) % 2 == 0:
-            x = str(s)
-            out.append(int(x[:len(x) // 2]))
-            out.append(int(x[len(x) // 2:]))
-        else:
-            out.append(s * 2024)
-    return out
+def blink(stone):
+    if stone == 0:
+        yield 1
+        return
+    s = str(stone)
+    n = len(s)
+    if n % 2 == 0:
+        yield int(s[:n // 2])
+        yield int(s[n // 2:])
+    else:
+        yield stone * 2024
 
 
-def part_1(file_name, n=25):
-    stones = load_data(file_name)
-    for i in range(n):
-        stones = blink(stones)
-    return len(stones)
+def gen_stone_list(stones, depth):
+    for _ in range(depth):
+        stones = chain(*(blink(stone) for stone in stones))
+    return list(stones)
+
+
+def part_1(file_name, depth):
+    return len(gen_stone_list(load_data(file_name), depth))
 
 
 # Part Two
-
-def blinky(stones):
-    for s in stones:
-        if s == 0:
-            yield 1
-        elif len(str(s)) % 2 == 0:
-            x = str(s)
-            yield int(x[:len(x) // 2])
-            yield int(x[len(x) // 2:])
-        else:
-            yield s * 2024
-
 cache = {}
 
-def recurse(stones, n):
-    if n == 0:
-        return len(stones)
-    total = 0
-    for s in blinky(stones):
-        if (s, n-1) not in cache:
-            cache[(s, n-1)] = recurse([s], n-1)
-        total += cache[(s, n-1)]
-    return total
+
+def calc_num_stones(stone, depth):
+    if depth == 0:
+        return 1
+    if (depth, stone) not in cache:
+        cache[(depth, stone)] = sum(calc_num_stones(s, depth - 1) for s in blink(stone))
+    return cache[(depth, stone)]
 
 
-def part_2(file_name, n=75):
-    stones = load_data(file_name)
-    return recurse(stones, n)
+def part_2(file_name, depth):
+    return sum(calc_num_stones(stone, depth) for stone in load_data(file_name))
 
 
 if __name__ == "__main__":
-    print("1. Example:", part_1("data/day11-example.txt"), "=? 55312")
-    print("1. Answer:", part_1("data/day11-data.txt"))
+    print("1. Example:", part_1("data/day11-example.txt", 25), "=? 55312")
+    print("1. Answer:", part_1("data/day11-data.txt", 25))
     print("2. Example:", part_2("data/day11-example.txt", 25), "=? 55312")
-    print("2. Answer:", part_2("data/day11-data.txt"))
+    print("2. Answer:", part_2("data/day11-data.txt", 75))
+    print("Cached nodes: ", len(cache))
