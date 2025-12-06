5f18bc4e1251ce552c19fba87d91168d9695a5bd
Bram Stoeller <bram.stoeller@brainbuilders.nl>
2025-12-06 07:08:08 +0100

    Revert "Grid utils"
    This reverts commit e8a7add9e9a407f9c4aefe6f1eee4b7401dbe53c.i
    I don't like having utils, it feels like cheating, as I developed the grid utils 'offline'. It is basically an external library.

--- aoc2025/day04.py
+++ aoc2025/day04.py
@@ -1,46 +1,50 @@
 # Advent of Code 2025, Day 4
 # https://adventofcode.com/2025/day/4
 
-from grid import convolve, center, count_if, count
-
 
 def load_data(input_file: str):
     return [list(line) for line in open(input_file).read().strip().split("\n")]
 
 
 # Part One
-def can_move(grid, roi):
-    ctr = center(grid, roi)
-    is_roll = ctr == "@"
-    return is_roll and count_if(grid, "@", roi) <= 4
+def count_nbh(grid, x, y):
+    count = 0
+    for dx in [-1, 0, 1]:
+        for dy in [-1, 0, 1]:
+            if dx == 0 and dy == 0:
+                continue
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < len(grid[0]) and 0 <= ny < len(grid):
+                count += grid[ny][nx] == "@"
+    return count
 
 
 def part_1(input_file):
     grid = load_data(input_file)
-
-    moved_map = convolve(grid, fn=can_move)
-    n_moved = count(moved_map)
-    return n_moved
+    count_grid = [[0] * len(grid[0]) for _ in range(len(grid))]
+    for y in range(len(grid)):
+        for x in range(len(grid[0])):
+            count_grid[y][x] = grid[y][x] != "." and count_nbh(grid, x, y) < 4
+    return sum(sum(row) for row in count_grid)
 
 
 # Part Two
-def move(grid, roi):
-    ctr = center(grid, roi)
-    is_roll = ctr == "@"
-    can_be_moved = is_roll and count_if(grid, "@", roi) <= 4
-    return can_be_moved, ("." if can_be_moved else ctr)
-
-
 def part_2(input_file):
     grid = load_data(input_file)
-
+    count_grid = [[0] * len(grid[0]) for _ in range(len(grid))]
     total = 0
     while True:
-        moved_map = convolve(grid, fn=move, update=True)
-        n_moved = count(moved_map)
-        if n_moved == 0:
+        for y in range(len(grid)):
+            for x in range(len(grid[0])):
+                count_grid[y][x] = grid[y][x] != "." and count_nbh(grid, x, y) < 4
+        for y in range(len(grid)):
+            for x in range(len(grid[0])):
+                if count_grid[y][x]:
+                    grid[y][x] = "."
+        x = sum(sum(row) for row in count_grid)
+        if x == 0:
             break
-        total += n_moved
+        total += x
     return total
 
 
