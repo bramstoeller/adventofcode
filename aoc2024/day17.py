8b4ef3afd307f50fb263b818fd5c7a45f449b0a3
Bram Stoeller <bram.stoeller@brainbuilders.nl>
2025-12-07 07:27:11 +0100

    cleanup

--- aoc2024/day17.py
+++ aoc2024/day17.py
@@ -21,25 +21,32 @@ combos = {
 }
 
 
-# The adv instruction (opcode 0) performs division. The numerator is the value in the A register. The denominator is found by raising 2 to the power of the instruction's combo operand. (So, an operand of 2 would divide A by 4 (2^2); an operand of 5 would divide A by 2^B.) The result of the division operation is truncated to an integer and then written to the A register.
+# The adv instruction (opcode 0) performs division. The numerator is the value in the A register. The denominator is
+# found by raising 2 to the power of the instruction's combo operand. (So, an operand of 2 would divide A by 4 (2^2);
+# an operand of 5 would divide A by 2^B.) The result of the division operation is truncated to an integer and then
+# written to the A register.
 def adv(reg, op):
-    reg[0] = reg[0] // (2 ** combos[op](reg))
+    reg[0] = reg[0] >> combos[op](reg)
     reg[-1] += 2
 
 
-# The bxl instruction (opcode 1) calculates the bitwise XOR of register B and the instruction's literal operand, then stores the result in register B.
+# The bxl instruction (opcode 1) calculates the bitwise XOR of register B and the instruction's literal operand, then
+# stores the result in register B.
 def bxl(reg, op):
     reg[1] = reg[1] ^ op
     reg[-1] += 2
 
 
-# The bst instruction (opcode 2) calculates the value of its combo operand modulo 8 (thereby keeping only its lowest 3 bits), then writes that value to the B register.
+# The bst instruction (opcode 2) calculates the value of its combo operand modulo 8 (thereby keeping only its lowest
+# 3 bits), then writes that value to the B register.
 def bst(reg, op):
     reg[1] = combos[op](reg) % 8
     reg[-1] += 2
 
 
-# The jnz instruction (opcode 3) does nothing if the A register is 0. However, if the A register is not zero, it jumps by setting the instruction pointer to the value of its literal operand; if this instruction jumps, the instruction pointer is not increased by 2 after this instruction.
+# The jnz instruction (opcode 3) does nothing if the A register is 0. However, if the A register is not zero, it jumps
+# by setting the instruction pointer to the value of its literal operand; if this instruction jumps, the instruction
+# pointer is not increased by 2 after this instruction.
 def jnz(reg, op):
     if reg[0] == 0:
         reg[-1] += 2
@@ -47,27 +54,31 @@ def jnz(reg, op):
         reg[-1] = op
 
 
-# The bxc instruction (opcode 4) calculates the bitwise XOR of register B and register C, then stores the result in register B. (For legacy reasons, this instruction reads an operand but ignores it.)
+# The bxc instruction (opcode 4) calculates the bitwise XOR of register B and register C, then stores the result in
+# register B. (For legacy reasons, this instruction reads an operand but ignores it.)
 def bxc(reg, _):
     reg[1] = reg[1] ^ reg[2]
     reg[-1] += 2
 
 
-# The out instruction (opcode 5) calculates the value of its combo operand modulo 8, then outputs that value. (If a program outputs multiple values, they are separated by commas.)
+# The out instruction (opcode 5) calculates the value of its combo operand modulo 8, then outputs that value. (If a
+# program outputs multiple values, they are separated by commas.)
 def out(reg, op):
     reg[-1] += 2
     return combos[op](reg) % 8
 
 
-# The bdv instruction (opcode 6) works exactly like the adv instruction except that the result is stored in the B register. (The numerator is still read from the A register.)
+# The bdv instruction (opcode 6) works exactly like the adv instruction except that the result is stored in the
+# B register. (The numerator is still read from the A register.)
 def bdv(reg, op):
-    reg[1] = reg[0] // (2 ** combos[op](reg))
+    reg[1] = reg[0] >> combos[op](reg)
     reg[-1] += 2
 
 
-# The cdv instruction (opcode 7) works exactly like the adv instruction except that the result is stored in the C register. (The numerator is still read from the A register.)
+# The cdv instruction (opcode 7) works exactly like the adv instruction except that the result is stored in the
+# C register. (The numerator is still read from the A register.)
 def cdv(reg, op):
-    reg[2] = reg[0] // (2 ** combos[op](reg))
+    reg[2] = reg[0] >> combos[op](reg)
     reg[-1] += 2
 
 
