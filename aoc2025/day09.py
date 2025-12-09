9097fcaef40f321db6dc5d7a22f8d537a377ee0d
Bram Stoeller <bram.stoeller@brainbuilders.nl>
2025-12-09 15:28:10 +0100

    2025 Day 9: properly implement 'in polygon' check

--- aoc2025/day09.py
+++ aoc2025/day09.py
@@ -1,5 +1,6 @@
 # Advent of Code 2025, Day 9
 # https://adventofcode.com/2025/day/9
+from bisect import bisect
 from dataclasses import dataclass
 
 
@@ -27,91 +28,44 @@ def part_1(input_file):
 
 
 # Part Two
-def node_in_polygon(p, vertices) -> bool:
-    n = len(vertices)
+def sorted_edges(vertices):
+    edges = list(zip(vertices, vertices[1:] + vertices[:1]))
+    vertical = ((a.x, min(a.y, b.y), max(a.y, b.y)) for a, b in edges if a.x == b.x)
+    horizontal = ((a.y, min(a.x, b.x), max(a.x, b.x)) for a, b in edges if a.y == b.y)
+    return sorted(vertical), sorted(horizontal)
 
-    def edges():
-        return ((vertices[i], vertices[(i + 1) % n]) for i in range(n))
 
-    def between(a: int, b: int, v: int) -> bool:
-        return a <= v <= b if a <= b else b <= v <= a
+def intersect_any_edges(p0, p1, vertical, horizontal):
+    x0, x1 = min(p0.x, p1.x), max(p0.x, p1.x)
+    y0, y1 = min(p0.y, p1.y), max(p0.y, p1.y)
 
-    def on_edge(p: Tile, a: Tile, b: Tile) -> bool:
-        if a.x == b.x:
-            return p.x == a.x and between(a.y, b.y, p.y)
-        return p.y == a.y and between(a.x, b.x, p.x)
-
-    for a, b in edges():
-        if on_edge(p, a, b):
+    lower_bound = bisect(vertical, (x0 + 1,))
+    upper_bound = bisect(vertical, (x1,))
+    for _, edge_y_min, edge_y_max in vertical[lower_bound:upper_bound]:
+        if edge_y_min < y1 and edge_y_max > y0:
             return True
 
-    inside = False
-    x, y = p.x, p.y
+    lower_bound = bisect(horizontal, (y0 + 1,))
+    upper_bound = bisect(horizontal, (y1,))
+    for _, edge_x_min, edge_x_max in horizontal[lower_bound:upper_bound]:
+        if edge_x_min < x1 and edge_x_max > x0:
+            return True
 
-    for a, b in edges():
-        if a.y == b.y:
-            continue
-        y_min = min(a.y, b.y)
-        y_max = max(a.y, b.y)
-        if y_min <= y < y_max:
-            if a.x > x:
-                inside = not inside
-
-    return inside
-
-
-def in_polygon(a: Tile, b: Tile, vertices) -> bool:
-    x0, x1 = min(a.x, b.x), max(a.x, b.x)
-    y0, y1 = min(a.y, b.y), max(a.y, b.y)
-
-    corners = [Tile(x0, y0), Tile(x1, y0), Tile(x1, y1), Tile(x0, y1)]
-    for p in corners:
-        if not node_in_polygon(p, vertices):
-            return False
-
-    for d in [10001, 1001, 101, 11, 1]:
-        for x in range(x0, x1 + 1, d):
-            p0 = Tile(x, y0)
-            p1 = Tile(x, y1)
-            if not node_in_polygon(p0, vertices) or not node_in_polygon(p1, vertices):
-                return False
-
-        for y in range(y0, y1 + 1, d):
-            p0 = Tile(x0, y)
-            p1 = Tile(x0, y)
-            if not node_in_polygon(p0, vertices) or not node_in_polygon(p1, vertices):
-                return False
-
-    return True
-
-
-def plot_polygon(a, b, boxes):
-    import matplotlib.pyplot as plt
-
-    x0, x1 = min(a.x, b.x), max(a.x, b.x)
-    y0, y1 = min(a.y, b.y), max(a.y, b.y)
-
-    poly_x = [p.x for p in boxes] + [boxes[0].x]
-    poly_y = [p.y for p in boxes] + [boxes[0].y]
-    rect_x = [x0, x1, x1, x0, x0]
-    rect_y = [y0, y0, y1, y1, y0]
-
-    plt.plot(poly_x, poly_y, "b")
-    plt.plot(rect_x, rect_y, "r-")
-
-    plt.grid()
-    plt.show()
+    return False
 
 
 def part_2(input_file):
     tiles = load_data(input_file)
+    vertical, horizontal = sorted_edges(tiles)
+
     rectangles = ((a, b) for i, a in enumerate(tiles) for b in tiles[i + 1 :])
-    rectangles = reversed(sorted(rectangles, key=lambda rect: area(*rect)))
+    rectangles = sorted(rectangles, key=lambda rect: area(*rect), reverse=True)
 
     for p0, p1 in rectangles:
-        if in_polygon(p0, p1, tiles):
-            plot_polygon(p0, p1, tiles)
+        if not intersect_any_edges(p0, p1, vertical, horizontal):
             return area(p0, p1)
+
+    return None
 
 
 if __name__ == "__main__":
